e8c8acae9de406c8c7dc773d368b25615554f56d
Carrie Gardner <carriegardner428@gmail.com>
2019-01-13 12:36:21 -0500

    update to form

--- project/frontend/src/components/Form.js
+++ project/frontend/src/components/Form.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+
 class Form extends Component {
   static propTypes = {
     endpoint: PropTypes.string.isRequired
   };
   state = {
-    name: {
-      placeholder: 'What is your name?',
-    },
+    name: "",
     email: "",
     message: ""
   };
